fc3dc2f34a4504182e5d4c60d5a21984fd48faec
Raymond Brown <rbrown@capitalent.com>
2025-09-27 13:34:44 -0500

    fix: add iOS SwiftUI project as normal folder (not submodule)

---
+++ apps/ios/SelectDo/SelectDo/SelectDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelectDoApp.swift
+//  SelectDo
+//
+//  Created by Raymond Brown on 9/27/25.
+//
+
+import SwiftUI
+
+@main
+struct SelectDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ apps/ios/SelectDo/SelectDoTests/SelectDoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SelectDoTests.swift
+//  SelectDoTests
+//
+//  Created by Raymond Brown on 9/27/25.
+//
+
+import Testing
+@testable import SelectDo
+
+struct SelectDoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
